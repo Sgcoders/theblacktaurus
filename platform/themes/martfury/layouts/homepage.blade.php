48bd893663da5c3df22ad05a8fe91de9c8ccb504
Sgcoders <88477477+Sgcoders@users.noreply.github.com>
2021-11-26 23:16:26 +0900

    youtube video attach

--- platform/themes/martfury/layouts/homepage.blade.php
+++ platform/themes/martfury/layouts/homepage.blade.php
@@ -88,6 +88,7 @@
                 </div>
                 <div class="modal-body">
                     <div class="embed-responsive embed-responsive-16by9">
+                        
                         <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/nTLY6ibdRh4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
 
