3950609de565d20e6a56c0ea719512025d2fd093
Sgcoders <kytham@sgcoders.com>
2021-11-28 09:55:40 +0900

    front-end fix

--- platform/themes/martfury/views/ecommerce/includes/product-items-grid.blade.php
+++ platform/themes/martfury/views/ecommerce/includes/product-items-grid.blade.php
@@ -3,9 +3,9 @@
         {!! Theme::partial('product-category-item', compact('product')) !!}
     </div>
 @empty
-    <div class="alert alert-warning mt-4 w-100" role="alert">
-        {{ __(':total Product found', ['total' => 0]) }}
-    </div>
+{{--    <div class="alert alert-warning mt-4 w-100" role="alert">--}}
+{{--        {{ __(':total Product found', ['total' => 0]) }}--}}
+{{--    </div>--}}
 @endforelse
 {{--<div class="ps-pagination">--}}
 {{--    {!! $products->withQueryString()->links() !!}--}}
